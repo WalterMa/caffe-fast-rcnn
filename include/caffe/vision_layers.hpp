eb2564a154ab07306bd4f4df92652b61e30978f6
Georgia <gkioxari@berkeley.edu>
2015-09-12 19:49:08 -0700

    mil, sum, weighted logistic and their headers

--- include/caffe/vision_layers.hpp
+++ include/caffe/vision_layers.hpp
@@ -453,6 +453,61 @@ class CuDNNPoolingLayer : public PoolingLayer<Dtype> {
 };
 #endif
 
+/*
+ * @brief Sums all bottom blobs
+ */
+template <typename Dtype>
+class SumLayer : public Layer<Dtype>{
+  public:
+  explicit SumLayer(const LayerParameter& param)
+    : Layer<Dtype>(param) {}
+
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top){}
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual ~SumLayer(){}
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+  virtual inline int MinNumBottomBlobs() const {return 1; }
+  virtual inline const char* type() const { return "Sum"; }
+  protected:
+    virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+    virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+    virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+    virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+};
+
+/* 
+ * @brief max of bottom blobs
+ */
+template <typename Dtype>
+class MILLayer : public Layer<Dtype> {
+ public:
+  explicit MILLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual inline int ExactNumBottomBlobs() const { return 1; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+  virtual inline const char* type() const { return "MIL"; }
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+  int context_per_roi_;
+  int num_rois_;
+  int channels_;
+};
+
 /**
  * @brief Does spatial pyramid pooling on the input image
  *        by taking the max, average, etc. within regions
